4363045012b4ca2ffedf8de27a86616d73ce4374
macienty <m.smietnik1@gmail.com>
2015-05-21 19:34:44 +0200

    Init

---
+++ Kontrola wersji oraz GUI Git/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontrola_wersji_oraz_GUI_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
